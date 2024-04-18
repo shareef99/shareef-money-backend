4cc7030aa904a5c4a8ea0d24694a1df3b00a00e9
Nadeem Shareef <nadeemshareef934@gmail.com>
2024-04-18 16:55:07 +0530

    chore: add exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import { NestFactory } from "@nestjs/core";
+import { HttpAdapterHost, NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { AllExceptionFilter } from "src/all-exceptions.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+
+  app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
   app.setGlobalPrefix("api/v1");
-  await app.listen(6000);
+  await app.listen(9000, () => console.log("Server is running on port 6000"));
 }
 bootstrap();
