97c48df12e1b2db107e4cb90d10817509b769f23
Nadeem Shareef <nadeemshareef934@gmail.com>
2024-09-12 14:58:47 +0530

    migrate the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { SubCategoriesModule } from "./sub-categories/sub-categories.module";
 import { Category } from "src/categories/categories.model";
 import { SubCategory } from "src/sub-categories/sub-categories.model";
 import { TransactionsModule } from "./transactions/transactions.module";
-// import { Sequelize } from "sequelize-typescript";
+import { Sequelize } from "sequelize-typescript";
 
 @Module({
   imports: [
@@ -32,21 +32,21 @@ import { TransactionsModule } from "./transactions/transactions.module";
   providers: [AppService],
 })
 export class AppModule {
-  // constructor() {
-  //   this.syncDatabase();
-  // }
-  // sequelize = new Sequelize({
-  //   dialect: "sqlite", // Specify your database dialect (e.g., sqlite, mysql, postgres)
-  //   storage: "database/database.sqlite", // Path to your SQLite database file
-  //   models: [User, Account, Category, SubCategory],
-  // });
-  // // Sync models with the database
-  // async syncDatabase() {
-  //   try {
-  //     await this.sequelize.sync({ alter: true });
-  //     console.log("Database synchronized successfully.");
-  //   } catch (error) {
-  //     console.error("Error syncing database:", error);
-  //   }
-  // }
+  constructor() {
+    this.syncDatabase();
+  }
+  sequelize = new Sequelize({
+    dialect: "sqlite", // Specify your database dialect (e.g., sqlite, mysql, postgres)
+    storage: "database/database.sqlite", // Path to your SQLite database file
+    models: [User, Account, Category, SubCategory],
+  });
+  // Sync models with the database
+  async syncDatabase() {
+    try {
+      await this.sequelize.sync({ alter: true });
+      console.log("Database synchronized successfully.");
+    } catch (error) {
+      console.error("Error syncing database:", error);
+    }
+  }
 }
