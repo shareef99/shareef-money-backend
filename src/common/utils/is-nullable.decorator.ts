538c1672730e6d57d65fac057f57eb71c1581d39
shareef-s7 <nadeemshareef934@gmail.com>
2025-06-01 02:15:30 +0530

    remove nestjs

--- src/common/utils/is-nullable.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { ValidateIf, ValidationOptions } from "class-validator";
-
-export function IsNullable(validationOptions?: ValidationOptions) {
-  return ValidateIf((_object, value) => value !== null, validationOptions);
-}
